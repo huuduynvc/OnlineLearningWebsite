9366578abcef4c757033e3ba4a2484fb49ecd08c
unknown <minhduy0310@gmail.com>
2020-12-16 01:07:57 +0700

    update login

---
+++ routes/account.route.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const moment = require('moment');
+
+const userModel = require('../../models/user.model');
+const auth = require('../../middlewares/auth.mdw');
+
+const router = express.Router();
+
+router.get('/login', async function (req, res) {
+  if (req.headers.referer) {
+    req.session.retUrl = ref;
+  }
+  res.render('vwAccount/login', {
+    layout: false
+  });
+})
+
+router.post('/login', async function (req, res) {
+  const user = await userModel.singleByUserName(req.body.username);
+  if (user === null) {
+    return res.render('vwAccount/login', {
+      layout: false,
+      err_message: 'Invalid username or password.'
+    });
+  }
+
+  const ret = bcrypt.compareSync(req.body.password, user.password);
+  if (ret === false) {
+    return res.render('vwAccount/login', {
+      layout: false,
+      err_message: 'Invalid username or password.'
+    });
+  }
+
+  req.session.isAuth = true;
+  req.session.authUser = user;
+
+  let url = req.session.retUrl || '/';
+  res.redirect(url);
+})
+
+router.post('/logout', async function (req, res) {
+  req.session.isAuth = false;
+  req.session.authUser = null;
+  res.redirect(req.headers.referer);
+})
+
+router.get('/register', async function (req, res) {
+  res.render('vwAccount/register');
+})
+
+router.post('/register', async function (req, res) {
+  const hash = bcrypt.hashSync(req.body.password, 10);
+  const dob = moment(req.body.dob, 'DD/MM/YYYY').format('YYYY-MM-DD');
+  const user = {
+    username: req.body.username,
+    password: hash,
+    dob: dob,
+    name: req.body.name,
+    email: req.body.email,
+    permission: 0
+  }
+
+  await userModel.add(user);
+  res.render('vwAccount/register');
+})
+
+router.get('/is-available', async function (req, res) {
+  const username = req.query.user;
+  const user = await userModel.singleByUserName(username);
+  if (user === null) {
+    return res.json(true);
+  }
+
+  res.json(false);
+})
+
+router.get('/profile', auth, async function (req, res) {
+  res.render('vwAccount/profile');
+})
+
+module.exports = router;
