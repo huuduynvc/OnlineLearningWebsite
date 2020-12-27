9d9f1a394e8a598f916ebbd0bb78ff02b034e9e3
unknown <minhduy0310@gmail.com>
2020-12-28 04:55:35 +0700

    fix login logout

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const hbs_sections = require('express-handlebars-sections');
 const session = require('express-session');
+require('express-async-errors');
 //create app
 const app = express();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
